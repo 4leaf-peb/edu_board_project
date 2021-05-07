4b234ad67c75f4439e15c2d9bbc8f1fa6d04755b
eunbee <4leaf.peb@gmail.com>
2021-05-07 15:54:32 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# LOL Board Project For Education
+
+<pre>
+    Node Express Web Server And View engine Pug With DB that MySQL
+</pre>
+
+## SCRENNS
+
+- [ ] Home Screen("/")
+- [ ] Board List Screen ("/board/list")
+- [ ] Board Write Screen ("/board/create")
+- [ ] Board Detail Screen ("/board/detao;?id=${id}")
+
+## LOGICS
+
+- [ ] SELECT BOARD
+- [ ] SELECT EMP
+- [ ] INSERT INTO BOARD
+- [ ] UPDATE BOARD
+- [ ] DELETE BOARD
---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import morgan from "morgan";
+import path from "path";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+
+// handler = controller 가기 전에 body-parser를 들린다.
+// post 방식일때 데이터를 가져올 수 있다.
+
+const app = express();
+const PORT = 7000;
+
+app.set("view engine", "pug");
+app.use(helmet());
+app.use(morgan(`dev`));
+
+app.use(express.static(path.join(__dirname, "/assets")));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+//암호화해서 가져온 data를 json형태로 보여주는 기능
+
+app.listen(PORT, () => {
+  console.log(`🌟 LOL_BOARD WEB PROPJECT - http://localhost"${PORT}`);
+});
